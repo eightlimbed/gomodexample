508a2f6fa4df9b9b15e532d03d2d347bae4b61f8
Lee Gaines <lee_gaines@apple.com>
2019-06-10 19:54:36 -0700

    init commit

---
+++ cmd/gomodexample/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/fatih/color"
+
+func PrintInRed(msg string) {
+	color.Red(msg)
+}
+
+func PrintInGreen(msg string) {
+	color.Green(msg)
+}
+
+func PrintInYellow(msg string) {
+	color.Yellow(msg)
+}
+
+func PrintInBlue(msg string) {
+	color.Blue(msg)
+}
